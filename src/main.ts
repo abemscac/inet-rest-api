79608d0ce44428b2886825ff0a1d896652786c59
abemscac <abe.msc.ac@gmail.com>
2022-03-08 21:56:07 +0800

    fix: replace GetNotFoundInterceptor with repository.findOneOrFail

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
-import { GetNotFoundInterceptor } from './interceptors/get-not-found.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -13,7 +12,6 @@ async function bootstrap() {
     new FastifyAdapter({ logger: true }),
   )
   app.useGlobalPipes(new ValidationPipe())
-  app.useGlobalInterceptors(new GetNotFoundInterceptor())
   app.setGlobalPrefix('api')
   await app.listen(33823, '0.0.0.0')
 }
