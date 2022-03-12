d9ec2a759695156ad95c5b9140953b527a2ad141
abemscac <abe.msc.ac@gmail.com>
2022-03-12 21:38:59 +0800

    fix: tinyint-boolean transformer NaN

--- src/transformers/tinyint-boolean.transformer.ts
+++ src/transformers/tinyint-boolean.transformer.ts
@@ -1,11 +1,21 @@
 import { ValueTransformer } from 'typeorm'
 
+interface ITinyIntBooleanTransformerOptions {
+  /**
+   * @default false
+   */
+  nullable: boolean
+}
+
 export class TinyIntBooleanTransformer implements ValueTransformer {
-  from(value: number | null): boolean | null {
-    return value === null ? null : value === 1
+  constructor(private readonly options?: ITinyIntBooleanTransformerOptions) {}
+
+  from(dbValue: number | null): boolean | null {
+    return dbValue === null ? null : dbValue === 1
   }
 
-  to(value: boolean | null): number | null {
-    return value === null ? null : Number(value)
+  to(value?: boolean | null): number | null {
+    const isEmpty = value === null || value === undefined
+    return isEmpty && this.options?.nullable ? null : Number(value ?? 0)
   }
 }
