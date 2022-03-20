2d8d3241e583294779c984596124777e7342bc4f
abemscac <abe.msc.ac@gmail.com>
2022-03-20 18:28:16 +0800

    fix: user-browse-history findByQuery params pipe

--- src/modules/user-browse-history/user-browse-history.controller.ts
+++ src/modules/user-browse-history/user-browse-history.controller.ts
@@ -3,12 +3,15 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   ParseIntPipe,
   Post,
   Query,
   UseGuards,
 } from '@nestjs/common'
+import { PagableParamsValidationPipe } from 'src/pipes/pagable-params.validation.pipe'
 import { PagableParams } from 'src/shared-params/pagable.params'
 import { IPagableViewModel } from 'src/shared-view-models/i-pagable.view-model'
 import { AccessTokenAuthGuard } from '../auth/guards/access-token.guard'
@@ -17,7 +20,7 @@ import { UserBrowseHistoryService } from './user-browse-history.service'
 import { IUserBrowseHistoryViewModel } from './view-models/i-user-browse-history.view-model'
 
 @UseGuards(AccessTokenAuthGuard)
-@Controller('/user-browse-histories')
+@Controller('user-browse-histories')
 export class UserBrowseHistoryController {
   constructor(
     private readonly userBrowseHistoryService: UserBrowseHistoryService,
@@ -25,7 +28,7 @@ export class UserBrowseHistoryController {
 
   @Get()
   async findByQuery(
-    @Query() params: PagableParams,
+    @Query(PagableParamsValidationPipe) params: PagableParams,
   ): Promise<IPagableViewModel<IUserBrowseHistoryViewModel>> {
     return await this.userBrowseHistoryService.findByQuery(params)
   }
@@ -38,11 +41,13 @@ export class UserBrowseHistoryController {
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async deleteById(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return await this.userBrowseHistoryService.deleteById(id)
   }
 
   @Delete('clear')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async clear(): Promise<void> {
     return await this.userBrowseHistoryService.clear()
   }
