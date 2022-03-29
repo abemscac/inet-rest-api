9b44483e23b4e04628f06d5569cb3dea46df3ae0
abemscac <abe.msc.ac@gmail.com>
2022-03-29 21:06:31 +0800

    docs: swagger

---
+++ src/modules/auth/auth.constants.ts
@@ -0,0 +1,2 @@
+export const UsernameRegexp = new RegExp('^[a-zA-Z0-9_.]{4,50}$')
+export const PasswordRegexp = new RegExp('^[a-zA-Z0-9_.~!@#$%^&*()-+]{4,60}$')
--- src/modules/user/forms/user-create.form.ts
+++ src/modules/user/forms/user-create.form.ts
@@ -1,16 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Transform } from 'class-transformer'
-import { IsNotEmpty, Length } from 'class-validator'
+import { IsNotEmpty, Length, Matches } from 'class-validator'
+import { PasswordRegexp, UsernameRegexp } from '~/modules/auth/auth.constants'
 import { ApiPropertyImage } from '~/swagger-decorators/api-property-image'
 
 export class UserCreateForm {
+  @ApiProperty({
+    description: `RegExp: ${UsernameRegexp.source}`,
+  })
   @IsNotEmpty()
   @Length(4, 50)
-  @Transform((params) => params.value?.trim())
+  @Matches(UsernameRegexp)
   username: string
 
   @IsNotEmpty()
+  @ApiProperty({
+    description: `RegExp: ${PasswordRegexp.source}`,
+  })
   @Length(4, 60)
-  @Transform((params) => params.value?.trim())
+  @Matches(PasswordRegexp)
   password: string
 
   @Length(0, 50)
