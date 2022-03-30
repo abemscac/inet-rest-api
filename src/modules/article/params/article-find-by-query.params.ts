dd3508447abf8ed4d9cdc4e26d4ccc515298adee
abemscac <abe.msc.ac@gmail.com>
2022-03-30 21:03:32 +0800

    feat: find article by keyword

--- src/modules/article/params/article-find-by-query.params.ts
+++ src/modules/article/params/article-find-by-query.params.ts
@@ -3,21 +3,21 @@ import { Type } from 'class-transformer'
 import { IsEnum, IsInt, IsOptional } from 'class-validator'
 import { PagableParams } from '~/shared-params/pagable.params'
 
-export enum ArticleCreatedRange {
+export enum ArticleCreatedWithin {
   today = 'today',
   week = 'week',
   month = 'month',
   year = 'year',
 }
 
-export class ArticleFindTopByQueryParams extends PagableParams {
+export class ArticleFindByQueryParams extends PagableParams {
   @ApiPropertyOptional({
-    enum: ArticleCreatedRange,
+    enum: ArticleCreatedWithin,
     description: 'The time range articles were created.',
   })
-  @IsEnum(ArticleCreatedRange)
+  @IsEnum(ArticleCreatedWithin)
   @IsOptional()
-  created?: ArticleCreatedRange
+  createdWithin?: ArticleCreatedWithin
 
   @ApiPropertyOptional({
     description: 'Id of the article-category the articles belong to.',
@@ -26,4 +26,6 @@ export class ArticleFindTopByQueryParams extends PagableParams {
   @Type(() => Number)
   @IsOptional()
   categoryId?: number
+
+  keyword?: string
 }
