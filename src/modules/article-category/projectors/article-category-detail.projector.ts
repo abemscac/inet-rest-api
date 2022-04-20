b82b05988ff074bee04de447d851982e17f7648c
abemscac <abe.msc.ac@gmail.com>
2022-04-20 08:50:14 +0800

    feat: find article-category detail by code

--- src/modules/article-category/projectors/article-category-detail.projector.ts
+++ src/modules/article-category/projectors/article-category-detail.projector.ts
@@ -2,20 +2,38 @@ import { Repository } from 'typeorm'
 import { BaseProjector } from '~/base-projectors/base-projector'
 import { ImageSize, ImgurUtil } from '~/utils/imgur.util'
 import { ArticleCategory } from '../article-category.entity'
-import { IArticleCategoryViewModel } from '../view-models/i-article-category.view-model'
+import { IArticleCategoryDetailViewModel } from '../view-models/i-article-category-detail.view-model'
 
-export class ArticleCategoryProjector extends BaseProjector<
+export type IArticleCategoryDetailProjection = Pick<
   ArticleCategory,
-  IArticleCategoryViewModel
+  'code' | 'imageHash' | 'imageExt'
+> & {
+  newArticleCountToday: number
+}
+
+export class ArticleCategoryDetailProjector extends BaseProjector<
+  ArticleCategory,
+  IArticleCategoryDetailViewModel,
+  IArticleCategoryDetailProjection
 > {
   constructor(repository: Repository<ArticleCategory>, alias: string) {
     super(
       repository
         .createQueryBuilder(alias)
+        .leftJoin(
+          'article',
+          'article',
+          [
+            `${alias}.id = article.category_id`,
+            'article.createdAt >= CURDATE()',
+            'article.createdAt < (CURDATE() + INTERVAL 1 DAY)',
+          ].join(' AND '),
+        )
         .select([
           `${alias}.code AS code`,
           `${alias}.imageHash AS imageHash`,
           `${alias}.imageExt AS imageExt`,
+          `COUNT(article.id) AS newArticleCountToday`,
         ]),
       alias,
     )
@@ -27,6 +45,7 @@ export class ArticleCategoryProjector extends BaseProjector<
           ext: projection.imageExt,
           size: ImageSize.SmallSquare,
         }) ?? '',
+      newArticleCountToday: projection.newArticleCountToday,
     }))
   }
 }
